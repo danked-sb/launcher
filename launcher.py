f2acfc4545f21e355e4ba4b3ee63099785deb5f9
syskeyed <44981148+1x6@users.noreply.github.com>
2021-05-23 10:19:09 +0100

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -9,7 +9,7 @@ if mode == 'fast':
     print('This is the fast version, so expect a ban/blacklist after a few hours.')
     print('Downloading the latest version from Github...')
     fastCode = requests.get('https://raw.githubusercontent.com/danked-sb/danked/main/fast.py').text
-    fast_changelog = requests.get('https://github.com/danked-sb/danked/raw/main/fast_changelog.txt').text
+    fast_changelog = requests.get('https://github.com/danked-sb/danked/raw/main/cdn/fast_changelog.txt').text
     print('\n')
     print(f'''CHANGELOG: \n{fast_changelog}''')
     time.sleep(1)
@@ -19,7 +19,7 @@ elif mode == 'safe':
     print('Bans may still occur but it is much more stealthy.')
     print('Downloading the latest version from Github...')
     saferCode = requests.get('https://raw.githubusercontent.com/danked-sb/danked/main/safe.py').text
-    safe_changelog = requests.get('https://github.com/danked-sb/danked/raw/main/safe_changelog.txt').text
+    safe_changelog = requests.get('https://github.com/danked-sb/danked/raw/main/cdn/safe_changelog.txt').text
     print('\n')
     print(f'''CHANGELOG: \n{safe_changelog}''')
     time.sleep(1)
